1252475624610304c47c60ad86e6954c5dac3652
David Zeugner <67552061+david01843@users.noreply.github.com>
2026-02-01 16:21:41 +0300

    Add files via upload

--- vocab_desktop.js
+++ vocab_desktop.js
@@ -17,11 +17,10 @@ const controls = document.querySelectorAll("#controls p")
 
 const chars = "ё1234567890-=йцукенгшщзхъ\\фывапролджэячсмитьбю,"
 const codes = [192, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 189, 187, 81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 219, 221, 220, 65, 83, 68, 70, 71, 72, 74, 75, 76, 186, 222, 90, 88, 67, 86, 66, 78, 77, 188, 190, 191]
-var shift = false
 var active = null
 var audio = new SpeechSynthesisUtterance()
 audio.lang = "ru"
-var focus = 3e3
+var focus = 5
 data.forEach(i => {
 	var p = document.createElement("p")
 	p.innerHTML = i[0]
@@ -33,38 +32,14 @@ data.forEach(i => {
 	lists.appendChild(p)
 })
 
-controls[0].addEventListener("click", () => {readxlsx.click()})
-controls[1].addEventListener("click", () => {
-	var d = []
-	document.querySelectorAll("#words p").forEach(i => d.push([i.dataset.ans, i.innerHTML]))
-	const b = XLSX.utils.book_new()
-	const s = XLSX.utils.aoa_to_sheet(d)
-	XLSX.utils.book_append_sheet(b, s, "Sheet1")
-	XLSX.writeFile(b, "words_download.xlsx")
-})
-controls[2].addEventListener("click", () => {
+controls[0].addEventListener("click", () => {
 	Array.from(document.querySelectorAll("#words p")).forEach(i => i.remove())
 	cycle()
 })
-controls[3].addEventListener("click", () => {
-	if (focus == 5) {focus = 3e3; controls[3].innerHTML = "focus (off)"}
-	else {focus = 5; controls[3].innerHTML = "focus (on)"}
+controls[1].addEventListener("click", () => {
+	if (focus == 5) {focus = 3e3; controls[1].innerHTML = "focus (off)"}
+	else {focus = 5; controls[1].innerHTML = "focus (on)"}
 })
-const readxlsx = document.querySelector("input")
-readxlsx.onchange = (e1) => {
-	const f = e1.target.files[0]
-	if (!f) return
-	const r = new FileReader()
-	r.onload = (e2) => {
-		const w = XLSX.read(e2.target.result, {type: "array"})
-		const j = XLSX.utils.sheet_to_json(w.Sheets[w.SheetNames[0]], {header: 1})
-		for (let i = 0; i < j.length; i++) {addword(j[i][0].trim(), j[i][1].trim())}
-		shuffle()
-		cycle()
-	}
-	e1.target.value = ''
-	r.readAsArrayBuffer(f)
-}
 words = document.querySelector("#words")
 function addword(s1, s2) {
 	w = document.createElement("p")
@@ -108,7 +83,10 @@ document.addEventListener("keydown", (e) => {
 	else if (e.keyCode == 8) {answer.innerHTML = answer.innerHTML.slice(0, -1); check()}
 	else if (e.keyCode == 13) {
 		if (!active) {cycle()}
-		else if (answer.style.color == "limegreen") {if (shift) {active.remove()}; cycle()}
+		else if (answer.style.color == "limegreen") {
+			if (e.shiftKey) {active.remove()}
+			cycle()
+		}
 		else {answer.innerHTML = active.dataset.ans; check()}
 	}
 	else if (e.keyCode == 32) {answer.innerHTML += " "; check()}
